0f36c96bb587cc3c117ef4a0a855e8ed8be9373b
danyelhenrique <danyelhenriquefidel@gmail.com>
2019-11-30 19:11:56 -0300

    change port

---
+++ src/config/dotenv.js
@@ -0,0 +1,4 @@
+const dotenv = require("dotenv");
+const config = dotenv.config()
+
+module.exports = config;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('./config/dotenv')
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -5,16 +6,20 @@ const routes = require("./routes");
 const cors = require("cors");
 const path = require("path");
 
+const PORT = process.env.PORT || 3333
+
 app.use(express.json());
 app.use(cors());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 
 mongoose.connect(
-  "mongodb+srv://danyel:Da-1danyel@cluster0-l8thx.mongodb.net/semana09?retryWrites=true&w=majority",
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-l8thx.mongodb.net/semana09?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
 );
 app.use(routes);
-app.listen(3333);
+app.listen(PORT, () => {
+  console.log(`Server is runnig on ${PORT}`)
+});
